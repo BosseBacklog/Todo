8c678155a7b6bea50eac1c4745b3590f8c26a416
Anners <anners@mailzorz.com>
2019-12-18 12:46:28 +0100

    gjorde om listorna till metoder

--- src/todo/Attgöra.java
+++ src/todo/Attgöra.java
@@ -7,23 +7,23 @@ import java.util.Scanner;
 public class Attgöra {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String s;
-//		Metoder m = new Metoder();
+		Metoder m = new Metoder();
 //		hej.add("objekt1");
 		Scanner scan = new Scanner(System.in);
-		List todo = new ArrayList();
+//		List todo = new ArrayList();
+		
 		while (true) {
-
+			
 			s = scan.nextLine();
 			if (s.substring(0, 8).equals("todo.add")) { // 7 steg
 
-				todo.add(s.substring(9));
+				m.todo.add(s.substring(9));
 
 			}
 			if (s.equals("todo.show")) {
-				for (int i = 0; i < todo.size(); i++) {
-					System.out.println(todo.get(i).toString());
+				for (int i = 0; i < m.todo.size(); i++) {
+					System.out.println(m.todo.get(i).toString());
 
 				}
 
--- src/todo/Metoder.java
+++ src/todo/Metoder.java
@@ -1,4 +1,4 @@
-package todo;
+	package todo;
 
 import java.util.ArrayList;
 import java.util.List;
